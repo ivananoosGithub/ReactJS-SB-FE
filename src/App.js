e9760eedd128a728604bac3af102edd4ad352506
Ivan Kim Anoos <55166879+ivananoosGithub@users.noreply.github.com>
2023-02-15 20:47:32 +0800

    Latest update is adding useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodo] = useState([
+    {
+      rowNumber: 1,
+      rowDescription: "Get haircut123",
+      rowAssigned: "Kimoy",
+    },
+    {
+      rowNumber: 2,
+      rowDescription: "LOL123",
+      rowAssigned: "Mercy123",
+    },
+  ]);
+
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodos = {
+        rowNumber: todos.length + 1,
+        rowDescription: "HAHAHAHXDD",
+        rowAssigned: "LOLHAHXDD",
+      };
+      setTodo((todos) => [...todos, newTodos]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+          <a
+            name=""
+            id=""
+            className="btn btn-primary"
+            href="#"
+            role="button"
+            onClick={addTodo}
+          >
+            Add new todo
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
